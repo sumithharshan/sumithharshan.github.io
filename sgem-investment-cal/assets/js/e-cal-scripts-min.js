e0b839d04cac6373fef9ef4af9ef86461bd4eca7
Sumith Harshan <sumith@surge.global>
2022-07-18 21:56:08 +0530

    Update e-cal-scripts-min.js

--- sgem-investment-cal/assets/js/e-cal-scripts-min.js
+++ sgem-investment-cal/assets/js/e-cal-scripts-min.js
@@ -6,5 +6,5 @@
  } 
 //}
    var sgemjs = document.createElement('script');
-   sgemjs.setAttribute('src','https://sumithharshan.github.io/sgem-investment-cal/assets/js/e-cal-scripts-min-full.js');
+   sgemjs.setAttribute('src','https://sumithharshan.github.io/sgem-investment-cal/assets/js/e-cal-scripts-min-all.js');
    document.body.appendChild(sgemjs);
